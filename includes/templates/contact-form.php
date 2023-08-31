7133c66b35b8ebc98cdba0cbb7b55375a524226a
Abolfazl <abolfazlmarzban39@gmail.com>
2023-08-31 14:05:25 +0430

    sixth

--- includes/templates/contact-form.php
+++ includes/templates/contact-form.php
@@ -1,4 +1,4 @@
-<!-- <?php if (get_plugin_options('contact_plugin_active')) : ?> -->
+<!-- <?php if (get_plugin_options('contact_plugin_active')): ?> -->
 
     <div id="form_success" style="background: green; color: white;"></div>
     <div id="form_errorr" style="background: red; color: white;text-align:right;"></div>
@@ -30,7 +30,9 @@
             <input type="file" name="passport">
 
             <p style="color: red; text-align:right">توجه: در صورت تغییر اسم و فامیل لطفا نام قبلی را درج فرمایید</p>
-            <textarea name="changeName" id="changeName" cols="30" rows="10" placeholder="در این کادر نام قبلی خود را به همراهتوضیحات مربوط به این تغییر نام درج فرمایید." style="text-align: right;"></textarea>
+            <textarea name="changeName" id="changeName" cols="30" rows="10"
+                placeholder="در این کادر نام قبلی خود را به همراهتوضیحات مربوط به این تغییر نام درج فرمایید."
+                style="text-align: right;"></textarea>
             <div style="margin-top: 10px;">
                 <button id="next">بعدی</button>
             </div>
@@ -82,7 +84,8 @@
             <label for="birthDate" style="text-align: right; margin-top:20px;">تاریخ تولد میلادی</label>
             <input type="date" name="birthDate" id="birthDate" style="text-align: end;">
 
-            <label for="imigrationIntent" style="text-align: right; margin-top: 20px;">آیا تا به حال برای مهاجرت به آمریکا از مسیر دیگری اقدام کرده اید؟</label>
+            <label for="imigrationIntent" style="text-align: right; margin-top: 20px;">آیا تا به حال برای مهاجرت به آمریکا
+                از مسیر دیگری اقدام کرده اید؟</label>
             <div style="display: flex; align-items: end; flex-direction:column;">
                 <div style="display: flex; align-items:center;">
 
@@ -125,10 +128,11 @@
             </div>
 
 
-            <label for="livingPlace" style="text-align: right; margin-top: 20px;">کشور محل زندگی در حال حاضر به انگلیسی</label>
+            <label for="livingPlace" style="text-align: right; margin-top: 20px;">کشور محل زندگی در حال حاضر به
+                انگلیسی</label>
             <input type="text" name="livingPlace" id="livingPlace" style="margin-top: 10px;">
 
-            
+
             <label for="passport Picture" style="text-align: right; margin-top: 20px;">تصویر پاسپورت</label>
             <input type="file" name="passport">
 
@@ -157,20 +161,20 @@
         <div id="fourthStep" style="display: flex; flex-direction: column;">
             <label for="employmentRecords" style="text-align:right">اطلاعات سابقه کاری</label>
             <textarea name="employmentRecords" id="employmentRecords" cols="30" rows="10"
-                style="text-align: right; margin-top: 10px;"
-                placeholder="نام محل خدمت:
+                style="text-align: right; margin-top: 10px;" placeholder="نام محل خدمت:
                             نام سوپروایزر:
                             کشور محل خدمت:
                             ادرس محل خدمت:
                             تایم کاری ( نیمه وقت و یا تمام وقت)
-                            "
-            ></textarea>
+                            "></textarea>
 
-            <label for="daneshNameh" style="text-align:right; margin-top: 10px">تصویر با کیفیت دانشنامه (اصل و ترجمه)</label>
+            <label for="daneshNameh" style="text-align:right; margin-top: 10px">تصویر با کیفیت دانشنامه (اصل و
+                ترجمه)</label>
             <input type="file" name="daneshNameh" id="daneshNameh">
 
 
-            <label for="rizNomarat" style="text-align:right; margin-top: 10px">تصویر با کیفیت از تمام صفحات ریزنمرات (اصل و ترجمه)</label>
+            <label for="rizNomarat" style="text-align:right; margin-top: 10px">تصویر با کیفیت از تمام صفحات ریزنمرات (اصل و
+                ترجمه)</label>
             <input type="file" name="rizNomarat" id="rizNomarat">
 
 
@@ -181,21 +185,55 @@
             </div>
         </div>
         <div id="fifthStep" style="display: flex; flex-direction: column;">
-        <label for="bachelorplace" style="text-align:right; margin-top: 10px">کشور محل تحصیل لیسانس</label>
-        <input type="text" name="bachelorplace" id="bachelorplace">
+            <label for="bachelorplace" style="text-align:right; margin-top: 10px">کشور محل تحصیل لیسانس</label>
+            <input type="text" name="bachelorplace" id="bachelorplace">
 
-        <label for="bacheloraddress" style="text-align:right; margin-top: 10px">آدرس محل دانشگاه</label>
-        <textarea name="bacheloraddress" id="bacheloraddress" cols="30" rows="10"></textarea>
+            <label for="bacheloraddress" style="text-align:right; margin-top: 10px">آدرس محل دانشگاه</label>
+            <textarea name="bacheloraddress" id="bacheloraddress" cols="30" rows="10"></textarea>
 
-        <label for="bachelorstart" style="text-align:right; margin-top: 10px">تاریخ شروع تحصیل به میلادی</label>
-        <input type="date" name="bachelorstart" id="bachelorstart" style="text-align:right;">
+            <label for="bachelorstart" style="text-align:right; margin-top: 10px">تاریخ شروع تحصیل به میلادی</label>
+            <input type="date" name="bachelorstart" id="bachelorstart" style="text-align:right;">
 
-        <label for="bachelorend" style="text-align:right; margin-top: 10px">تاریخ فارغ التحصیلی به میلادی</label>
-        <input type="date" name="bachelorend" id="bachelorend" style="text-align:right;">
+            <label for="bachelorend" style="text-align:right; margin-top: 10px">تاریخ فارغ التحصیلی به میلادی</label>
+            <input type="date" name="bachelorend" id="bachelorend" style="text-align:right;">
+            <div style="margin-top: 10px;">
+                <button id="next">بعدی</button>
+                <button id="next">قبلی</button>
 
+            </div>
 
         </div>
+        <div id="sixthStep" style="display: flex; flex-direction: column;">
+        <div style="display: flex; align-items: end; flex-direction:column;">
+                <div style="display: flex; align-items:center;">
+
+                    <label for="havePreUni" style="margin-right: 10px;">دارای مدرک پیش دانشگاهی هستم</label>
+                    <input type="radio" name="havePreUni" value="havePreUni">
+                    <br>
+                </div>
+                <div style="display: flex; align-items:center;">
+                    <label for="nothavePreUni" style="margin-right: 10px;">مدرک پیش دانشگاهی ندارم</label>
+                    <input type="radio" name="nothavePreUni" value="nothavePreUni">
+                    <br>
+                </div>
+            </div>
+
+
+            <label for="" style="margin-top: 10px; text-align: right;">اطلاعات محل تحصیل</label>
+
+
+            <label for="schoolName" style="text-align:right; margin-top: 10px;">نام مدرسه</label>
+            <input type="text" name="schoolName" id="schoolName">
+            
+            <div style="margin-top: 10px;">
+                <button id="next">بعدی</button>
+                <button id="next">قبلی</button>
+
+            </div>
+
+        </div>
+
         <!-- <button type="submit">Submit</button> -->
     </form>
 
-<!-- <?php endif; ?> -->
+    <!-- <?php endif; ?> -->
